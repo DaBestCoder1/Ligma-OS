919e9cdc15ceaeeb8aa299a33c1a28b4c80d89e2
SnakeGameRetro <115974665+SnakeGameRetro@users.noreply.github.com>
2022-11-15 18:03:56 +1100

    Add files via upload

---
+++ os.py
@@ -0,0 +1,41 @@
+﻿import time
+import os
+
+print("""
+
+██╗░░░░░██╗░██████╗░███╗░░░███╗░█████╗░░░░░░░░█████╗░░██████╗
+██║░░░░░██║██╔════╝░████╗░████║██╔══██╗░░░░░░██╔══██╗██╔════╝
+██║░░░░░██║██║░░██╗░██╔████╔██║███████║█████╗██║░░██║╚█████╗░
+██║░░░░░██║██║░░╚██╗██║╚██╔╝██║██╔══██║╚════╝██║░░██║░╚═══██╗
+███████╗██║╚██████╔╝██║░╚═╝░██║██║░░██║░░░░░░╚█████╔╝██████╔╝
+╚══════╝╚═╝░╚═════╝░╚═╝░░░░░╚═╝╚═╝░░╚═╝░░░░░░░╚════╝░╚═════╝░
+
+""")
+
+print("""
+[1]Log in...
+[2]Already done with setup...
+[3]I haven't created an acount
+""")
+
+setup = input("[?]: ")
+
+if setup == '1':
+    name = input(str("Please Enter UserName/email: "))
+    pas = input(str("Please Enter Password: "))
+    os.startfile("home.py")
+
+if setup == '2':
+    os.startfile("home.py")
+
+if setup == '3':
+    os.startfile("Start.py")
+
+
+    
+    
+
+
+
+
+
